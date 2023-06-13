2363cbaddde03129524fd2550c1a0fb8126e7977
Raul iqbal <89615372+Rauliqbal@users.noreply.github.com>
2023-06-13 19:06:43 +0700

    add tutorial page

--- pages/tutorial/[id].vue
+++ pages/tutorial/[id].vue
@@ -2,9 +2,12 @@
 const route = useRoute().params.id;
 </script>
 <template>
-   <main>
-      <article class="container prose">
+   <main class="h-screen flex flex-col items-center justify-center">
+      <!-- <article class="container prose">
          <LazyContentDoc :path="'/tutorial/' + route" />
-      </article>
+      </article> -->
+
+      <img class="max-w-[580px] w-full" src="~/assets/images/404.png" alt="" />
+      <NuxtLink class="btn-primary" to="/tutorial">Kembali</NuxtLink>
    </main>
 </template>
