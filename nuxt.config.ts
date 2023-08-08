2d52af94e97cf013923b37deb4734a0bdf242372
Raul iqbal <89615372+Rauliqbal@users.noreply.github.com>
2023-08-08 18:28:35 +0700

    add google site verification

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,14 @@ export default defineNuxtConfig({
          charset: "utf-8",
          viewport: "width=device-width, initial-scale=1",
          link: [{ rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600&family=JetBrains+Mono&family=Poppins:wght@400;500;600&display=swap" }],
+         meta: [
+            {
+               name: "google-site-verification",
+               content: "Dh5WebMpXLjyWXTl9crOY7Y3rTQ7c1Bgfxyvvz6p1XM",
+            },
+         ],
       },
+
       pageTransition: { name: "page", mode: "out-in" },
    },
    css: ["~/assets/css/main.css"],
